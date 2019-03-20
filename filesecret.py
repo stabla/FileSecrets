104420d79b6949e0e5689f01180b11086979c8cf
bonnet <bonnet@mbp.local>
2019-03-20 15:19:55 +0100

    adding js & css to the crawler

--- filesecret.py
+++ filesecret.py
@@ -1,30 +1,56 @@
 
 import requests
-from bs4 import BeautifulSoup
+import re
 
+from bs4 import BeautifulSoup
 from selenium import webdriver
+
+import argparse
 from termcolor import colored
 
-url = 'http://guillaumebonnet.fr'
-ext = 'js'
-
-## In depth, you need chrome driver
-driver = webdriver.Chrome('driver/chromedriver')  # use Chrome driver for example
-
-def listFD(url, ext=''):
-    ##page = requests.get(url).text
+## find js files
+def getJSFiles(url):
+    page = requests.get(url).text
     ##print(page)
-    soup = BeautifulSoup(driver.page_source, 'html.parser')
-    js = [i.get('src') for i in soup.find_all('script') if i.get('src')]
+    soup = BeautifulSoup(page, 'html.parser')
+    print(colored("[*] Downloading js files...", "blue"))
+    js = [i.get('src') for i in soup.find_all('script') if (i.get('src') and re.search('.js$', i.get('src')))]
     return js
 
+## find css files
+def getCSSFiles(url):
+    page = requests.get(url).text
+    ##print(page)
+    soup = BeautifulSoup(page, 'html.parser')
+    print(colored("[*] Downloading css files...", "blue"))
+    css = [i.get('href') for i in soup.find_all('link') if (i.get('href') and re.search('.css$', i.get('href')))]
+    return css
 
 
-driver.get(url)
-for file in listFD(url, ext):
-    print(file)
 
+parser = argparse.ArgumentParser()
+parser.add_argument("url", help="target's url")
+parser.add_argument("--depth", help="increase precision of the research through a webdriver", default=0)
+args = parser.parse_args()
 
+print(colored("=====================================================", "blue"))
+print(colored(" FileSecrets                          v0.1  @stabla  ", "blue"))
+print(colored("=====================================================", "blue"))
 
-## TERMCOLOR ADDED
-## print(colored('hello', 'red'), colored('world', 'green'))
+if args.url:
+    url = args.url
+    if "http" not in args.url:
+        url = "http://" + args.url
+        
+    for jsFiles in getJSFiles(url):
+        print(jsFiles)
+    for cssFiles in getCSSFiles(url):
+        print(cssFiles)
+
+if args.depth:
+    ## In depth, you need chrome driver and it will do a better inspect with dynamic js files
+    print("depthturned on")
+    driver = webdriver.Chrome('driver/chromedriver')  # use Chrome driver for example
+    driver.get(url)
+    soup = BeautifulSoup(driver.page_source, 'html.parser')
+    print(colored("URL/Domain: Must be specified", "blue"))
