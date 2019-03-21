255aae79046018543b3731b63320d2a0efc05da5
Guillaume Bonnet <stabla@protonmail.com>
2019-03-21 14:08:49 +0100

    building digger

---
+++ binder/app.js
@@ -0,0 +1,117 @@
+function callPage() {
+    setTimeout(function () {
+        $aboutContainer.animate({
+            left: "50%",
+            width: "50%"
+        }), $aboutMeTitle.show(), $blockIndex.show(), $("body").removeClass("hidden-before-load"), $("html").removeClass("hidden-before-load")
+    }, 300), $(".mail-logo").on("click", function () {
+        callContact(), $contactPage.addClass("active")
+    }), $(".block--contact .close").on("click", function () {
+        $contactPage.removeClass("active"), existProjects()
+    }), $(".project-logo").on("click", function () {
+        callProjects(), $projectsPart.addClass("active")
+    }), $(".block--projects .close").on("click", function () {
+        $projectsPart.removeClass("active"), existProjects()
+    }), $(document).keyup(function (t) {
+        27 == t.which && $contactPage.hasClass("active pointer") && ($contactPage.removeClass("active"), $contactPage.hide())
+    }), $(".block--contact button").on("click", function (t) {
+        t.preventDefault()
+        var e = $(this),
+            o = e.parent(),
+            n = o.find("input[name='email_sender']").val(),
+            a = o.find("textarea[name='content_sender']").val()
+        
+        if (n.length > 0 && a.length > 0) {
+            e.prev().hide()
+            $.ajax({
+                type: "POST",
+                url: o.attr("action"),
+                data: {
+                    email_sender: n,
+                    content_sender: a
+                },
+                timeout: 4e3,
+                success: function (t) {
+                    e.css({
+                        "background-color": "green"
+                    }), e.html("Sent! Take a coffee and wait").fadeIn(999).attr('disabled', true)
+                },
+                error: function () {
+                    e.css({
+                        "background-color": "red"
+                    }), e.html("Something turned wrong, retry.").fadeIn(999)
+                }
+            })
+        } else {
+            e.prev().show();
+        }
+    })
+}
+var $welcomeContainer, $logoSubtitle, $aboutContainer, $aboutMeTitle, $blockIndex, $projectsPart, $contactPage
+const callProjects = function () {
+        $blockIndex.animate({
+            opacity: "0"
+        }, "slow"), setTimeout(function () {
+            $aboutContainer.animate({
+                left: "100%"
+            }), setTimeout(function () {
+                $projectsPart.show()
+            }, 300)
+        }, 450)
+    },
+    callContact = function () {
+        $blockIndex.animate({
+            opacity: "0"
+        }, "slow"), setTimeout(function () {
+            $aboutContainer.animate({
+                left: "100%"
+            }), setTimeout(function () {
+                $contactPage.show()
+            }, 300)
+        }, 450)
+    },
+    existProjects = function () {
+        $("body").addClass("hidden-before-load"), $("html").addClass("hidden-before-load"), $projectsPart.addClass("fadeOutDown"), $contactPage.addClass("fadeOutDown"), setTimeout(function () {
+            $aboutContainer.animate({
+                left: "50%"
+            }), setTimeout(function () {
+                $projectsPart.hide(), $contactPage.hide(), $projectsPart.removeClass("fadeOutDown"), $contactPage.removeClass("fadeOutDown"), $blockIndex.animate({
+                    opacity: "1"
+                }, "slow"), $("body").removeClass("hidden-before-load"), $("html").removeClass("hidden-before-load")
+            }, 300)
+        }, 450)
+    }
+$(document).ready(function () {
+    $aboutContainer = $(".block--index"), $aboutMeTitle = $(".block--index .title"), $blockIndex = $(".block--index .animate"), $projectsPart = $(".block--projects"), $contactPage = $(".block--contact"), $(".listizi").attr("src", "projects/Screen%20Shot%202016-12-23%20at%2019.04.22.png"), $(".soundColor").attr("src", "projects/Screen%20Shot%202016-12-23%20at%2019.05.25.png"), $(".toolslist").attr("src", "projects/Screen%20Shot%202017-02-20%20at%2019.35.14.png"), $(function () {
+        var t = ($(".js-tilt").tilt(), $(".js-tilt-output").tilt())
+        t.on("change", function (t, e) {
+            var o = $(this).closest(".js-parent").find(".js-output")
+            $("<li><strong>X: </strong>" + e.percentageX + " | <strong>Y: </strong>" + e.percentageY + "</li>").prependTo(o)
+        }), $(".js-destroy").on("click", function () {
+            var t = $(this).closest(".js-parent").find(".js-tilt")
+            t.tilt.destroy.call(t)
+        }), $(".js-enable").on("click", function () {
+            var t = $(this).closest(".js-parent").find(".js-tilt")
+            t.tilt()
+        })
+    }), $(".mail-logo").removeAttr("href").css("cursor", "pointer"), $(function () {
+        function t(t) {
+            var a = "visible",
+                c = "hidden",
+                i = {
+                    focus: a,
+                    focusin: a,
+                    pageshow: a,
+                    blur: c,
+                    focusout: c,
+                    pagehide: c
+                }
+            t = t || window.event, t.type in i ? (e = n, $(document).attr("title", e)) : (e = this[o] ? "Hey! Where are you?" : n, $(document).attr("title", e))
+        }
+        var e, o = "hidden",
+            n = document.title
+        o in document ? document.addEventListener("visibilitychange", t) : (o = "mozHidden") in document ? document.addEventListener("mozvisibilitychange", t) : (o = "webkitHidden") in document ? document.addEventListener("webkitvisibilitychange", t) : (o = "msHidden") in document ? document.addEventListener("msvisibilitychange", t) : "onfocusin" in document ? document.onfocusin = document.onfocusout = t : window.onpageshow = window.onpagehide = window.onfocus = window.onblur = t, void 0 !== document[o] && t({
+            type: document[o] ? "blur" : "focus"
+        })
+    }), callPage()
+})
--- filesecret.py
+++ filesecret.py
@@ -6,33 +6,34 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys  
 from selenium.webdriver.chrome.options import Options  
 
+downloadedFiles = []
+
 parser = argparse.ArgumentParser()
 parser.add_argument("url", help="target's url")
 parser.add_argument("-d", "--depth", help="increase precision by checking dynamic files (require webdrive)", default=False)
 parser.add_argument("-e", "--external", help="increase precision by analyzing external libraries", default=False)
 args = parser.parse_args()
 
+
 ## find js files
 def getJSFiles(url):
     page = requests.get(url).text
-    ##print(page)
     soup = BeautifulSoup(page, 'html.parser')
-    print(colored("[*] Downloading js files...", "blue"))
+    print(colored("[*] Retrieving js files...", "blue"))
     js = [i.get('src') for i in soup.find_all('script') if (i.get('src') and re.search('.js$', i.get('src')))]
     return js
 
 ## find css files
 def getCSSFiles(url):
     page = requests.get(url).text
-    ##print(page)
     soup = BeautifulSoup(page, 'html.parser')
-    print(colored("[*] Downloading css files...", "blue"))
+    print(colored("[*] Retrieving css files...", "blue"))
     css = [i.get('href') for i in soup.find_all('link') if (i.get('href') and re.search('.css$', i.get('href')) and sameDomain(i.get('href'), url))]
     return css
 
+## verify if the file is hosted on the given URL
 def sameDomain(fileUrl, initialUrl):
     domainName = tldextract.extract(initialUrl).domain
-
     ## is it starting with www/https/http?
     if (re.search("^(http|https)://", fileUrl) or re.search("^www.", fileUrl)):
         ## check the domain name and compare to initialUrl if it's from the same domain
@@ -41,26 +42,51 @@ def sameDomain(fileUrl, initialUrl):
             return False
     return True
 
+## download a file
 def downloadLocally(fileUrl, initialUrl):
     if sameDomain(fileUrl, initialUrl):
         ## download on intiialUrl + fileUrl
         url = initialUrl + fileUrl
         file_name = getFileURI(fileUrl)
+        print(colored("[*] Download file:" + file_name, "blue"))
         # Download the file from `url` and save it locally under `file_name`:
         urllib.request.urlretrieve(url, "binder/" + file_name)
+
         
+## get the filename with its extension
 def getFileURI(URL):
     filename_w_ext = os.path.basename(URL)
     filename, file_extension = os.path.splitext(filename_w_ext)
     return filename + '' + file_extension
 
-print(colored(downloadLocally("js/app.js", "https://guillaumebonnet.fr/"), "green"))        
+## open the and search in it
+def digger(fileName):
+    lineCpt = 0
+    with open("binder/" + fileName) as openfile:
+        for line in openfile:
+            lineCpt = lineCpt + 1
+            for part in line.split():
+                if "pointer" in part:
+                    print(colored(" [*] Found : at line " + str(lineCpt) + " in " + fileName, "red"))
 
+digger("app.js")        
+
+###############
+## user menu ##
+###############
 print(colored("=====================================================", "blue"))
 print(colored(" FileSecrets                          v0.1  @stabla  ", "blue"))
-print(colored("=====================================================", "blue"))
 
+## check the url given as argument
 if args.url:
+    print(colored("=====================================================", "blue"))
+    print(colored(" url : " + args.url, "blue"))
+    a = 'true' if args.depth else 'false'
+    print(colored(" depth : " + a, "blue"))
+
+    b = 'true' if args.external else 'false'
+    print(colored(" external : " + b, "blue"))
+    print(colored("=====================================================", "blue"))
     url = args.url
     if "http" not in args.url:
         url = "http://" + args.url
@@ -70,9 +96,9 @@ if args.url:
     for cssFiles in getCSSFiles(url):
         print(cssFiles)
 
+## check the depth given as argument (optional)
+## In depth, you need chrome driver and it will do a better inspect with dynamic js file
 if args.depth:
-    ## In depth, you need chrome driver and it will do a better inspect with dynamic js files
-    print("depthturned on")
     options = Options()
     options.headless = True
     driver = webdriver.Chrome('driver/chromedriver', options=options)  # use Chrome driver for example
@@ -82,6 +108,6 @@ if args.depth:
     soup = BeautifulSoup(driver.page_source, 'html.parser')
     print(colored("[*] Downloading css files...", "blue"))
     js = [i.get('src') for i in soup.find_all('script') if (i.get('src') and re.search('.js$', i.get('src')) and sameDomain(i.get('src'), url))]
-    for j in js:
-        print(j)
+    ## for j in js:
+        ## print(j)
 
