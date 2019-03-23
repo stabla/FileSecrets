44e5d920d8364601034b8e77965f0d0c09337378
Guillaume Bonnet <stabla@protonmail.com>
2019-03-23 14:25:50 +0100

    error if no dic nor word specified

--- filesecret.py
+++ filesecret.py
@@ -1,4 +1,5 @@
-import os, requests, re, tldextract, argparse, shutil, time
+import os, requests, re, tldextract, argparse, shutil, requests
+import mimetypes
 import urllib.request
 from bs4 import BeautifulSoup
 from termcolor import colored
@@ -6,15 +7,25 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys  
 from selenium.webdriver.chrome.options import Options  
 
+def str2bool(v):
+    if v.lower() in ('yes', 'true', 't', 'y', '1'):
+        return True
+    elif v.lower() in ('no', 'false', 'f', 'n', '0'):
+        return False
+    else:
+        raise argparse.ArgumentTypeError('Boolean value expected.')
+
 parser = argparse.ArgumentParser()
 parser.add_argument("url", help="target's url")
-parser.add_argument("-d", "--dynamic", help="increase precision by checking dynamic files (require webdriver)", default=False)
-parser.add_argument("-e", "--external", help="increase precision by analyzing external libraries", default=False)
-parser.add_argument("-k", "--keep", help="keep file when downloaded, do not remove them", default=False)
+parser.add_argument("-d", "--dynamic", help="increase precision by checking dynamic files (require webdriver)", type=str2bool, default=False)
+parser.add_argument("-e", "--external", help="increase precision by analyzing external libraries", type=str2bool, default=False)
+parser.add_argument("-k", "--keep", help="keep file when downloaded, do not remove them", type=str2bool, default=False)
 parser.add_argument("-w", "--word", help="add some words to the original library", default="")
+parser.add_argument("-dic", "--dictionnary", help="add some words to the original library", type=str2bool, default=True)
 args = parser.parse_args()
 
 downloadedFiles = []
+folder = 'binder/'
 
 ## find js files
 def getJSFiles(url):
@@ -31,7 +42,7 @@ def getCSSFiles(url):
 ## find html, php files files
 def getHTMLFiles(url):
     print(colored("[*] Retrieving html files...", "blue"))
-    html = [i.get('href') for i in soup.find_all('a') if (i.get('href') and re.search('.php$|.html$', i.get('href')) and (sameDomain(i.get('href'), url)))]
+    html = [i.get('href') for i in soup.find_all('a') if (i.get('href') and re.search('.php|.html$', i.get('href')) and (sameDomain(i.get('href'), url)))]
     for i in soup.find_all('a'):
         if (i.get('href') and re.search('.*$', i.get('href')) and (sameDomain(i.get('href'), url) == False)):
             print(colored("     [!] There's a pointer to an external website: " + i.get('href') + " - Not going further", "grey", attrs=['bold']))
@@ -40,13 +51,12 @@ def getHTMLFiles(url):
 ## find all others, with no extension
 def getOtherFiles(url):
     print(colored("[*] Retrieving other files...", "blue"))
-    other = [i.get('href') for i in soup.find_all('a') if (i.get('href') and re.search('^([^.]+)$', i.get('href')) and (sameDomain(i.get('href'), url)))]
-    print(other)
-    ##print(other)
-    ##for i in other:
-      ##  print(i)
-        ##if i != '#':
-          ##  urllib.request.urlretrieve(i, "binder/" + os.path.splitext("path_to_file")[0] + ".txt")
+    other = [i.get('href') for i in soup.find_all('a') if (i.get('href') and re.search('^(?!mailto:|ftp://|^#$|.*\.css$|.*\.html|.*\.php)', i.get('href')) and (sameDomain(i.get('href'), url)))]
+    for i in other:
+        last_url_part = i.rsplit('/', 1)
+        print("     [+] " + last_url_part[1] + " (unknown extension)")
+        urllib.request.urlretrieve(i, folder + last_url_part[1] + ".txt")
+        downloadedFiles.append(last_url_part[1] + ".txt")
     
 ## verify if the file is hosted on the given URL
 def sameDomain(fileUrl, initialUrl):
@@ -72,27 +82,29 @@ def downloadLocally(fileUrl, initialUrl):
         url = initialUrl + fileUrl
         file_name = getFileURI(fileUrl)
         downloadedFiles.append(file_name)
-        print("to be downloaded:" + file_name)
         # Download the file from `url` and save it locally under `file_name`:
-        urllib.request.urlretrieve(url, "binder/" + file_name)
-        print("downloaded:" + file_name)
+        urllib.request.urlretrieve(url, folder + file_name)
 
 ## download the homepage
 def downloadOrigin(intiialUrl):
     url = intiialUrl
     downloadedFiles.append("HOME_PAGE.html")
-    urllib.request.urlretrieve(url, "binder/" + "HOME_PAGE.html")
+    urllib.request.urlretrieve(url, folder + "HOME_PAGE.html")
+
 
 ## open the and search in it
 def digger(fileName):
-    with open("secrets.txt", "r") as f: words = [line.rstrip('\n \t') for line in f]
-    if args.word:
+    words = []
+    if (args.dictionnary == True):
+        with open("secrets.txt", "r") as f: words = [line.rstrip('\n \t') for line in f]
+    if (args.word != ""):
         just_word = [x.strip() for x in args.word.split(',')]
         words.extend(just_word)
-    regex = "\\b" + "\\b|\\b".join(words) + "\\b"
-    for i, line in enumerate(open('binder/' + fileName)):
-        for match in re.finditer(regex, line, flags=re.IGNORECASE):
-            print(colored("         [*] Found word : '" + match.group(0) + "' at line " + str(i+1) + " in " + fileName, "red"))
+    else:
+        regex = "\\b" + "\\b|\\b".join(words) + "\\b"
+        for i, line in enumerate(open(folder + fileName)):
+            for match in re.finditer(regex, line, flags=re.IGNORECASE):
+                print(colored("         [*] Found word : '" + match.group(0) + "' at line " + str(i+1) + " in " + fileName, "red"))
 
 ## used as middleware, should be used first to correctly format url
 def formatUrlInput(url):
@@ -109,7 +121,70 @@ def formatUrlInput(url):
 print(colored("=====================================================", "blue"))
 print(colored(" FileSecrets                          v0.1  @stabla  ", "blue"))
 
-if args.url:
+def main():
+    if args.url:
+
+        downloadOrigin(url)
+
+        ## check the dynamic given as argument (optional)
+        ## In dynamic, you need chrome driver and it will do a better inspect with dynamic js file
+        if args.dynamic:
+            print(colored("=====================================================", "blue"))
+            print (colored("Running Headless Chrome Initialized", "blue"))
+            options = Options()
+            options.headless = True
+            driver = webdriver.Chrome('driver/chromedriver', options=options)  # use Chrome driver for example
+            driver.set_window_position(-10000,0)
+            driver.get(url)
+            soup = BeautifulSoup(driver.page_source, 'html.parser')
+
+        for jsFiles in getJSFiles(url):
+            print("     [+] " + jsFiles)
+            downloadLocally(jsFiles, url)
+        print(colored("[OK] Downloaded js files. ", "blue"))
+
+        for cssFiles in getCSSFiles(url):
+            print("     [+] " + cssFiles)
+            downloadLocally(cssFiles, url)
+        print(colored("[OK] Downloaded css files. ", "blue"))
+
+        for htmlFiles in getHTMLFiles(url):
+            print("     [+] " + htmlFiles)
+            downloadLocally(htmlFiles, url)
+        print(colored("[OK] Downloaded html files. ", "blue"))
+
+        getOtherFiles(url)
+        print(colored("[OK] Downloaded all other files... ", "blue"))
+
+        print(colored("[*] List of files currently downloaded locally", "blue"))
+        print("     "  + str(downloadedFiles))
+        
+        print(colored("[*] Starting the digging in files... ", "blue"))
+
+        if ((args.dictionnary == False) and (args.word == "")):
+            print(colored("/!\ ERROR: NO DICTIONNARY AND NO WORDS.", "magenta"))
+        else:
+            ## digging in found file
+            for i in downloadedFiles:
+                print(colored("     [-] Checking in " + i, "blue"))
+                digger(i)
+            
+            ## delete all files in the folder binder/
+            if (args.keep == False):
+                print(colored("[*] Deleting all files downloaded previously", "blue"))
+                for the_file in os.listdir(folder):
+                    file_path = os.path.join(folder, the_file)
+                    try:
+                        if os.path.isfile(file_path):
+                            os.unlink(file_path)
+                    except Exception as e:
+                        print(e)
+    else:
+        print(colored("/!\ ERROR: NO URL.", "magenta"))
+
+if ((args.dictionnary == False) and (args.word == "")):
+    print(colored("/!\ ERROR: NO DICTIONNARY AND NO WORDS.", "magenta"))
+else:
     url = args.url
     print(colored("=====================================================", "blue"))
     print(colored(" url : " + args.url, "blue"))
@@ -118,6 +193,15 @@ if args.url:
 
     b = 'true' if args.external else 'false'
     print(colored(" external : " + b, "blue"))
+
+    c = 'true' if args.keep else 'false'
+    print(colored(" keep files : " + c, "blue"))
+
+    d = 'false' if (args.word == '') else 'true'
+    print(colored(" custom words : " + d, "blue"))
+
+    e = 'true' if (args.dictionnary == True) else 'false'
+    print(colored(" use dictionnary : " + e, "blue"))
     print(colored("=====================================================", "blue"))
 
     ## format url
@@ -129,58 +213,6 @@ if args.url:
 
     page = requests.get(url).text
     soup = BeautifulSoup(page, 'html.parser')
-
-    downloadOrigin(url)
-
-   ## check the dynamic given as argument (optional)
-    ## In dynamic, you need chrome driver and it will do a better inspect with dynamic js file
-    if args.dynamic:
-        print(colored("=====================================================", "blue"))
-        print (colored("Running Headless Chrome Initialized", "blue"))
-        options = Options()
-        options.headless = True
-        driver = webdriver.Chrome('driver/chromedriver', options=options)  # use Chrome driver for example
-        driver.set_window_position(-10000,0)
-        driver.get(url)
-        soup = BeautifulSoup(driver.page_source, 'html.parser')
-
-    for jsFiles in getJSFiles(url):
-        print("     [+] " + jsFiles)
-        downloadLocally(jsFiles, url)
-    print(colored("[OK] Downloaded js files. ", "blue"))
-
-    for cssFiles in getCSSFiles(url):
-        print("     [+] " + cssFiles)
-        downloadLocally(cssFiles, url)
-    print(colored("[OK] Downloaded css files. ", "blue"))
-
-    for htmlFiles in getHTMLFiles(url):
-        print("     [+] " + htmlFiles)
-        downloadLocally(htmlFiles, url)
-    print(colored("[OK] Downloaded html files. ", "blue"))
-
-    getOtherFiles(url)
-    print(colored("[OK] Downloaded all other files... ", "blue"))
-
-    print(colored("[*] List of files currently downloaded locally", "blue"))
-    print("     "  + str(downloadedFiles))
     
-    print(colored("[*] Starting the digging in files... ", "blue"))
-    for i in downloadedFiles:
-        print(colored("     [-] Checking in " + i, "blue"))
-        digger(i)
-else:
-    print(colored("/!\ ERROR: NO URL.", "purple"))
 
-## delete all files in the folder binder/
-if (args.keep == False):
-    folder = 'binder/'
-    for the_file in os.listdir(folder):
-        file_path = os.path.join(folder, the_file)
-        try:
-            if os.path.isfile(file_path):
-                os.unlink(file_path)
-        except Exception as e:
-            print(e)
-
-## try something else
+    main()
